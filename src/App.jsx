216776d01b0df5cdea1aab9d373f0d3a77fc5fdb
Salma Gitesatani <salmagitesatani@Salmas-MacBook-Pro.local>
2026-07-20 18:37:15 -0700

    Made base for front page

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+
+function App() {
+    return (
+        <div
+            style={{
+                position: "fixed",
+                top: 0,
+                left: 0,
+                width: "100vw",
+                height: "100vh",
+                backgroundColor: "#F3F1EA",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: "8px",
+            }}
+        >
+            <h1 style={{ margin: 0, fontSize: "2rem", fontWeight: 700, colour: "#111111" }}>
+                Welcome to Sprout
+            </h1>
+            <p style={{ margin: 0, fontSize: "1rem", fontWeight: 400, colour: "#666666" }}>
+                Add your first plant to get started. Sprout will build your care schedule and remind you what to do and when.
+            </p>
+        </div>
+    );
+}
+
+export default App;
